5b96a63b53e5a858f93b4b252e152f12a7d125f7
goran <goran.djermanovic@neomatic.io>
2025-01-19 21:31:51 +0100

    init

---
+++ src/test/java/rs/services/mssc_brewery_client/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.services.mssc_brewery_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
