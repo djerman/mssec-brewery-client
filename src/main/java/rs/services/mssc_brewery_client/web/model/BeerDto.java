283772c48294a9b98cf87f91715941ee78ddf44e
goran <goran.djermanovic@neomatic.io>
2025-02-17 19:45:35 +0100

    beerDto

--- src/main/java/rs/services/mssc_brewery_client/web/model/BeerDto.java
+++ src/main/java/rs/services/mssc_brewery_client/web/model/BeerDto.java
@@ -2,6 +2,9 @@ package rs.services.mssc_brewery_client.web.model;
 
 import java.util.UUID;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +16,12 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
+    @NotBlank
     private String beerName;
+    @NotBlank
     private String beerStyle;
+    @Positive
     private Long upc;
 }
